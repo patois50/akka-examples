a1923a5b75ae1c577ba8c9e11fd3d8c6105e5489
Patrick McGeever <patois50@gmail.com>
2016-01-13 15:47:28 +0000

    Fixed broken reference for the question mark operator.

--- ask-example/src/main/scala/com/patrickmcgeever/scala/akka/askexample/AskNameMessage.scala
+++ ask-example/src/main/scala/com/patrickmcgeever/scala/akka/askexample/AskNameMessage.scala
@@ -2,6 +2,7 @@ package com.patrickmcgeever.scala.akka.askexample
 
 import akka.actor.{Actor, ActorSystem, Props}
 import akka.pattern
+import akka.pattern.ask
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
@@ -22,16 +23,13 @@ object AskTest extends App {
   val myActor = system.actorOf(Props[TestActor], name = "myActor")
 
   // (1) One way to ask an actor
-  /**
-   * Looks like the question mark operator has been deprecated or moved
-   *
+
   val future = myActor ? AskNameMessage
-  val result = Await.result(future, timeout.duration).asInstanceOf[String]
+  val result = Await.result(future, (5 seconds)).asInstanceOf[String]
   println(result)
 
-    */
 
-  // (2) this is a slightly different way to ask another actor
+    // (2) this is a slightly different way to ask another actor
   val future2: Future[String] = pattern.ask(myActor, AskNameMessage("Patrick"))(5 seconds).mapTo[String]
   val result2 = Await.result(future2, 1 second)
   println(result2)
